a1d6142e4064d67d650153bcc0686c03318e36fd
root <root@kaliZala.zalastiel>
2020-01-20 06:14:47 -0300

    install express to serve as server

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (request, response)=> {
-    return response.send('Hello Jovens')
+app.use(express.json())
+//métodos http: get , post , put, delete
+
+//tipos de parametros:
+//query params: request.query (filtros, ordenacao, paginacao, ...)
+//route params: request.params (Identificar um recurso na alteracao ou na remocao)
+//body params: request.body (Dados para criação ou alteração de um registro)
+app.post('/users', (request, response)=> {
+    console.log(request.body)
+    return response.json({message: 'Heya Jovens'})
 })
 
 app.listen(3333);
